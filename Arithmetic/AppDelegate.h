9600015cc5387655891b3b0b76268ab5a757e75c
zekun.li <zekun.li@luckincoffee.com>
2020-05-14 14:05:54 +0800

    Initial Commit

---
+++ Arithmetic/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Arithmetic
+//
+//  Created by ZeKunLi on 2020/5/14.
+//  Copyright © 2020 luckin coffee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Arithmetic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Arithmetic
+//
+//  Created by ZeKunLi on 2020/5/14.
+//  Copyright © 2020 luckin coffee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
